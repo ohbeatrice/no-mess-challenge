bd9a7f157364017f4886391f6dbf29ee02599886
Beatrice <ohbeatrice@gmail.com>
2023-09-03 18:26:46 -0400

    Chore List Add
    Added Chore List

---
+++ helpers.js
@@ -0,0 +1,69 @@
+// Complete chores list along with their categories
+let chores = [
+  { name: 'Clean bedroom', category: 'Daily Chores' },
+  { name: 'Do laundry', category: 'Daily Chores' },
+  { name: 'Clean bathroom', category: 'Daily Chores' },
+  { name: 'Unload and load dishwasher or do dishes in the sink', category: 'Daily Chores' },
+  // ... rest of the Daily Chores
+  { name: 'Shovel snow', category: 'Seasonal Chores' },
+  { name: 'Rake leaves', category: 'Seasonal Chores' },
+  // ... rest of the Seasonal Chores
+  { name: 'Vacuum under couch cushions', category: 'Deep Cleaning Chores' },
+  { name: 'Shampoo carpets', category: 'Deep Cleaning Chores' },
+  // ... rest of the Deep Cleaning Chores
+];
+
+// Function to list chores
+const listChores = () => {
+  return chores;
+};
+
+// Function to display chores
+const displayChores = () => {
+  try {
+    const cleverResponses = [
+      "🌟 Chore-o-vision: Presenting the Chorescape Saga! 🪄\n",
+      "🎉 Chore-tastic Adventure Awaits! Your Chores of the Day:\n",
+      "🪚 The Chore Chronicles Unfold: Dive into Today's Tasks!\n",
+      "🚀 Chore Quest: Embark on a Journey through Household Heroism!\n",
+      "🔮 The Enchanted Choreboard: Where Tasks Transform into Triumphs!\n"
+    ];
+    
+    const randomResponse = cleverResponses[Math.floor(Math.random() * cleverResponses.length)];
+
+    if (chores.length === 0) {
+      return '🏖 No chores for today! Enjoy your free time!';
+    }
+
+    const categorizedChores = chores.reduce((acc, chore) => {
+      acc[chore.category] = acc[chore.category] || [];
+      acc[chore.category].push(chore.name);
+      return acc;
+    }, {});
+
+    let choreList = '';
+    for (const [category, choreNames] of Object.entries(categorizedChores)) {
+      choreList += `**${category}**\n${choreNames.join('\n')}\n\n`;
+    }
+
+    return `${randomResponse}${choreList}`;
+  } catch (err) {
+    console.error("Error in displayChores:", err);
+    return 'An error occurred while fetching the chores.';
+  }
+};
+
+// Function to display leaderboard (stub)
+const displayLeaderboard = () => {
+  try {
+    // Your code to display the leaderboard
+  } catch (error) {
+    console.error("Error in displayLeaderboard:", error);
+  }
+};
+
+module.exports = {
+  listChores,
+  displayChores,
+  displayLeaderboard
+};
--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
 require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
+const APPLICATION_ID = process.env.APPLICATION_ID
+const TOKEN = process.env.TOKEN
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+const GUILD_ID = process.env.GUILD_ID
 
-
-const axios = require('axios')
+const axios = require('axios');
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
-
+const { listChores, displayChores, displayLeaderboard } = require('./helpers'); // Import the functions
 
 const app = express();
-// app.use(bodyParser.json());
 
 const discord_api = axios.create({
   baseURL: 'https://discord.com/api/',
   timeout: 3000,
   headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Authorization",
+    "Authorization": `Bot ${TOKEN}`
   }
 });
-
-
-
 
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
 
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
+    if (interaction.data.name === 'yo') {
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
@@ -43,36 +35,39 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
       });
     }
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
+    if (interaction.data.name === 'dm') {
+      let c = (await discord_api.post(`/users/@me/channels`, {
         recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
+      })).data;
+      try {
+        let dm_res = await discord_api.post(`/channels/${c.id}/messages`, {
+          content: 'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
+        });
+        console.log(dm_res.data);
+      } catch (e) {
+        console.log(e);
       }
-
       return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
+        data: {
+          content: '👍',
+        },
+      });
+    }
+
+    if (interaction.data.name === 'chores') {
+      const choreMessage = displayChores(); // Fetch the message from the displayChores function
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: choreMessage, // Send the fetched message as content
+        },
       });
     }
   }
-
 });
 
-
-
-app.get('/register_commands', async (req,res) =>{
+app.get('/register_commands', async (req, res) => {
   let slash_commands = [
     {
       "name": "yo",
@@ -83,31 +78,31 @@ app.get('/register_commands', async (req,res) =>{
       "name": "dm",
       "description": "sends user a DM",
       "options": []
+    },
+    {
+      "name": "chores",
+      "description": "lists today's chores",
+      "options": []
     }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
+  ];
+  try {
     let discord_response = await discord_api.put(
       `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
       slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
+    );
+    console.log(discord_response.data);
+    return res.send('commands have been registered');
+  } catch (e) {
+    console.error(e.code);
+    console.error(e.response?.data);
+    return res.send(`${e.code} error from discord`);
   }
-})
+});
 
-
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
-
+app.get('/', async (req, res) => {
+  return res.send('Follow documentation');
+});
 
 app.listen(8999, () => {
-
-})
-
+  console.log("Server is running on port 8999");
+});
