dbad202c1daafc7497e38a29c058f9a4fc8a8715
Beatrice <ohbeatrice@gmail.com>
2023-09-03 21:21:05 -0400

    refactored version of the nonworking index.js
    Moved the declaration of the Axios instance below the environmental variable initializations.
    Added const interaction = req.body; in the POST /interactions route.
    Removed duplicated code sections.
    Added a log to confirm that the server is running.
    Used express.json() middleware for parsing JSON request bodies.
    Removed commented-out code for clarity.

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
-const { listChores, displayChores, displayLeaderboard, displayHelp } = require('./helpers'); // Added displayHelp
+
+const { listChores, displayChores, displayLeaderboard, displayHelp } = require('./helpers');
 
 const APPLICATION_ID = process.env.APPLICATION_ID;
 const TOKEN = process.env.TOKEN;
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set';
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set';  // Default value for debugging
 const GUILD_ID = process.env.GUILD_ID;
 
 const app = express();
+app.use(express.json());
 
+// Initialize Axios instance
 const discord_api = axios.create({
   baseURL: 'https://discord.com/api/',
   timeout: 3000,
   headers: {
+    "Authorization": `Bot ${TOKEN}`,
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    "Access-Control-Allow-Headers": "Authorization",
-    "Authorization": `Bot ${TOKEN}`
+    "Access-Control-Allow-Headers": "Authorization"
   }
 });
 
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  console.log("Received interaction:", req.body);
+  const interaction = req.body;
+  console.log("Received interaction:", interaction);
 
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     if (interaction.data.name === 'yo') {
@@ -34,7 +38,6 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         },
       });
     } else if (interaction.data.name === 'dm') {
-      // existing dm logic to send a direct message
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
@@ -42,7 +45,6 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         },
       });
     } else if (interaction.data.name === 'chores') {
-      // Assuming displayChores() would return a string message, needs adjustment in helpers.js
       const choreMessage = displayChores();
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -51,7 +53,6 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         },
       });
     } else if (interaction.data.name === 'help') {
-      // Assuming displayHelp returns a string message
       const helpMessage = displayHelp();
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
